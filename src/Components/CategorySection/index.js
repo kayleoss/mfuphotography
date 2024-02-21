f48ad9378feb3ce1d1c9f1142133940914b6998e
Katie L <katie_acting@live.ca>
2024-02-21 17:09:10 -0500

    cleanup columns code

--- src/Components/CategorySection/index.js
+++ src/Components/CategorySection/index.js
@@ -26,10 +26,12 @@ const CategorySection = () => {
         
     }, [id])
     
-    var slicedLength = Math.round(state.images.length/3);
-    var colOne = state.images.slice(0, slicedLength)
-    var colTwo = state.images.slice(slicedLength, slicedLength*2)
-    var colThree = state.images.slice(slicedLength*2, state.images.length)
+    const slicedLength = Math.round(state.images.length/3);
+    const cols = [
+        state.images.slice(0, slicedLength), 
+        state.images.slice(slicedLength, slicedLength*2), 
+        state.images.slice(slicedLength*2, state.images.length)
+    ];
     
     return (
         <>
@@ -43,132 +45,47 @@ const CategorySection = () => {
                 </div>
                 : ""}
 
+                
                 <div className="row">
-                    <div className="col-sm-4">
-                    {colOne.map(image => (
-                        <div key={image._id}>
-                            <div className="modal fade" id={'modal-' + image._id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div className="modal-dialog modal-xl">
-                                    <div className="modal-content rounded-0">
-                                    <div className="modal-body p-0">
-                                        <img
-                                            src={image.src} 
-                                            alt={image.description}
-                                            className="w-100"
-                                        />
-                                    </div>
-                                    <div className="modal-footer">
-                                            <p><strong>{image.name}</strong> by Molly Fu</p>
-                                            <button type="button" className="btn-close px-2 py-3" data-bs-dismiss="modal" aria-label="Close"></button>
+                    {cols.map(col => (
+                        <div className="col-sm-4">
+                            {col.map(image => (
+                                <div key={image._id}>
+                                    <div className="modal fade" id={'modal-' + image._id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div className="modal-dialog modal-xl">
+                                            <div className="modal-content rounded-0">
+                                            <div className="modal-body p-0">
+                                                <img
+                                                    src={image.src} 
+                                                    alt={image.description}
+                                                    className="w-100"
+                                                />
+                                            </div>
+                                            <div className="modal-footer">
+                                                    <p><strong>{image.name}</strong> by Molly Fu</p>
+                                                    <button type="button" className="btn-close px-2 py-3" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                    
-                            
-                            <div>
-                                <button type="button" aria-label="View larger" className="btn p-0" data-bs-toggle="modal" data-bs-target={'#modal-' + image._id}>
-                                    <img
-                                        src={image.src} 
-                                        alt={image.description}
-                                        className="w-100  box-shadow"
-                                    />
-                                </button>
-                                
-                                <p className="mt-3">
-                                    <strong>{image.name}</strong><br/>
-                                    {image.description}<br/>
-                                    <a className="text-decoration-none" href={image.purchaseLink} target='_blank' rel="noreferrer">Purchase for ${image.price}</a>
-                                </p>
-                            </div>
-                        </div>
-                        
-                    ))}
-                    </div>
-
-                    <div className="col-sm-4">
-                    {colTwo.map(image => (
-                        <div key={image._id}>
-                            <div className="modal fade" id={'modal-' + image._id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div className="modal-dialog modal-xl">
-                                    <div className="modal-content rounded-0">
-                                    <div className="modal-body p-0">
-                                        <img
-                                            src={image.src} 
-                                            alt={image.description}
-                                            className="w-100"
-                                        />
-                                    </div>
-                                    <div className="modal-footer">
-                                            <p><strong>{image.name}</strong> by Molly Fu</p>
-                                            <button type="button" className="btn-close px-2 py-3" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
+                                    <div>
+                                        <button type="button" aria-label="View larger" className="btn p-0" data-bs-toggle="modal" data-bs-target={'#modal-' + image._id}>
+                                            <img
+                                                src={image.src} 
+                                                alt={image.description}
+                                                className="w-100  box-shadow"
+                                            />
+                                        </button>
+                                        <p className="mt-3">
+                                            <strong>{image.name}</strong><br/>
+                                            {image.description}<br/>
+                                            <a className="text-decoration-none" href={image.purchaseLink} target='_blank' rel="noreferrer">Purchase for ${image.price}</a>
+                                        </p>
                                     </div>
                                 </div>
-                            </div>
-                    
-                            
-                            <div>
-                                <button type="button" aria-label="View larger" className="btn p-0" data-bs-toggle="modal" data-bs-target={'#modal-' + image._id}>
-                                    <img
-                                        src={image.src} 
-                                        alt={image.description}
-                                        className="w-100  box-shadow"
-                                    />
-                                </button>
-                                
-                                <p className="mt-3">
-                                    <strong>{image.name}</strong><br/>
-                                    {image.description}<br/>
-                                    <a className="text-decoration-none" href={image.purchaseLink} target='_blank' rel="noreferrer">Purchase for ${image.price}</a>
-                                </p>
-                            </div>
-                        </div>
-                        
-                    ))}
+                            ))}
                     </div>
-
-                    <div className="col-sm-4">
-                    {colThree.map(image => (
-                        <div key={image._id}>
-                            <div className="modal fade" id={'modal-' + image._id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div className="modal-dialog modal-xl">
-                                    <div className="modal-content rounded-0">
-                                    <div className="modal-body p-0">
-                                        <img
-                                            src={image.src} 
-                                            alt={image.description}
-                                            className="w-100"
-                                        />
-                                    </div>
-                                    <div className="modal-footer">
-                                            <p><strong>{image.name}</strong> by Molly Fu</p>
-                                            <button type="button" className="btn-close px-2 py-3" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                    
-                            
-                            <div>
-                                <button type="button" aria-label="View larger" className="btn p-0" data-bs-toggle="modal" data-bs-target={'#modal-' + image._id}>
-                                    <img
-                                        src={image.src} 
-                                        alt={image.description}
-                                        className="w-100  box-shadow"
-                                    />
-                                </button>
-                                
-                                <p className="mt-3">
-                                    <strong>{image.name}</strong><br/>
-                                    {image.description}<br/>
-                                    <a className="text-decoration-none" href={image.purchaseLink} target='_blank' rel="noreferrer">Purchase for ${image.price}</a>
-                                </p>
-                            </div>
-                        </div>
-                        
                     ))}
-                    </div>
                 </div>
                 
             </div>
